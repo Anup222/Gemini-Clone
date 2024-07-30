b24c03b0cfb1f111362cdc24395a388670d95f1f
Anup Gorai <anupgorai51056@gmail.com>
2024-07-30 10:16:14 +0530

    .end added

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -4,8 +4,8 @@
 import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } from "@google/generative-ai";
 
 const MODEL_NAME = "gemini-1.0-pro";
-// const API_KEY = import.meta.env.VITE_API_KEY;
-const API_KEY = "AIzaSyCCBnUfSJCelhtdA0zRJfF4N2XFmhfDgyA";
+const API_KEY = import.meta.env.VITE_API_KEY;
+// const API_KEY = "AIzaSyCCBnUfSJCelhtdA0zRJfF4N2XFmhfDgyA";
 
 async function runChat(prompt) {
     const genAI = new GoogleGenerativeAI(API_KEY);
